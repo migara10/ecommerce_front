9de5d1668498f926077be66ac109ea55c188283d
migaa10 <migaraten@gmail.com>
2022-10-28 15:04:10 +0530

    set saver image url in img background

--- src/app/service/api-servise.service.ts
+++ src/app/service/api-servise.service.ts
@@ -6,7 +6,7 @@ import { Observable, throwError } from 'rxjs';
   providedIn: 'root'
 })
 export class ApiServiseService {
-  private API_URL = isDevMode() ? "http://localhost:3000/" : ""
+  public API_URL = isDevMode() ? "http://localhost:3000/" : ""
 
 
 
--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -7,7 +7,15 @@ import { ApiServiseService } from 'src/app/service/api-servise.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  /* public products = {
+    product_id: "",
+    product_name: "",
+    product_category: "",
+    product_disc: "",
+    product_imageuri: "",
+  }; */
+  products: any;
+  baseUri = this.api.API_URL;
   constructor(private api: ApiServiseService) { }
 
   ngOnInit(): void {
@@ -18,7 +26,9 @@ export class HomeComponent implements OnInit {
   getAllProducts() {
     this.api.getresponse("get", "product", {})
       .subscribe(res => {
-        console.log(res)
+        this.products = res.data;
+        console.log(this.products)
+        console.log(this.api.API_URL)
       },
         err => console.log(err)
 

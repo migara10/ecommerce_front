69dc65906708bec7e949e572326d3b7b9b964043
migaa10 <migaraten@gmail.com>
2022-11-10 22:46:30 +0530

    order compot add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
 import { CheckoutComponent } from './views/checkout/checkout.component';
+import { OrderListComponent } from './order/order-list/order-list.component';
 const routes: Routes = [
   {
     path: '',
@@ -13,6 +14,11 @@ const routes: Routes = [
     pathMatch: 'full',
     component: CheckoutComponent
   },
+  {
+    path: 'view_order',
+    pathMatch: 'full',
+    component: OrderListComponent,
+  }
 ];
 
 @NgModule({

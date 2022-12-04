f99533b9bfb223cb4544d64acd0ae273e2fe0968
migaa10 <migaraten@gmail.com>
2022-12-04 09:39:14 +0530

    add edit-peoduct-popup

---
+++ src/app/order/edit-product-popup/edit-product-popup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-product-popup',
+  templateUrl: './edit-product-popup.component.html',
+  styleUrls: ['./edit-product-popup.component.scss']
+})
+export class EditProductPopupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

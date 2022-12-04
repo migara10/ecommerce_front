774f7fa1b5affc5ddd34d4293031e4f6e752f378
migaa10 <migaraten@gmail.com>
2022-12-05 00:52:37 +0530

    add product edit popup

--- src/app/order/edit-product-popup/edit-product-popup.component.ts
+++ src/app/order/edit-product-popup/edit-product-popup.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { ApiServiseService } from 'src/app/service/api-servise.service';
+import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-edit-product-popup',
@@ -6,10 +11,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-product-popup.component.scss']
 })
 export class EditProductPopupComponent implements OnInit {
+  baseUri = this.api.API_URL;
+  selectedFile: any;
 
-  constructor() { }
+  constructor(private router: Router,public dialogRef: MatDialogRef<EditProductPopupComponent>, @Inject(MAT_DIALOG_DATA) public data: any, private api: ApiServiseService, private toastr: ToastrService) { }
 
+  productItem = new FormGroup({
+    product_id: new FormControl('', [Validators.required]),
+    product_name: new FormControl('', [Validators.required]),
+    product_category: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    product_disc: new FormControl('', [Validators.required]),
+    product_price: new FormControl('', [Validators.required]),
+  })
   ngOnInit(): void {
+    console.log(this.data)
+    this.productItem.setValue({
+      product_id: this.data.product_id,
+      product_name: this.data.product_name,
+      product_category: this.data.product_category,
+      product_disc: this.data.product_disc,
+      product_price: this.data.product_price,
+    })
+  }
+  saveNews() {
+    const fd = new FormData();
+    fd.append("file", this.selectedFile);
+    fd.append("product_id", this.productItem.value['product_id'] || '');
+    fd.append("product_name", this.productItem.value['product_name'] || '');
+    fd.append("product_category", this.productItem.value['product_category'] || '');
+    fd.append("product_disc", this.productItem.value['product_disc'] || '');
+    fd.append("product_price", this.productItem.value['product_price'] || '');
+
+    this.api.getresponse("put", "product", fd)
+      .subscribe(res => {
+        this.toastr.success(res.msg);
+        // this.router.navigate(['/add_items'] , { queryParams: { id: res.data.product_id}});
+      },
+        err => console.log(err)
+
+      )
+  };
+  onFileSelected = (event: any) => {
+    this.selectedFile = event.target.files[0];
+    console.log(this.selectedFile);
+  };
+  get f() {
+    return this.productItem.controls;
+  }
+  close() {
+    this.dialogRef.close();
   }
 
 }
--- src/app/order/edit-product/edit-product.component.ts
+++ src/app/order/edit-product/edit-product.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiServiseService } from 'src/app/service/api-servise.service';
-
+import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
+import { EditProductPopupComponent } from '../edit-product-popup/edit-product-popup.component';
 
 @Component({
   selector: 'app-edit-product',
@@ -11,7 +12,7 @@ export class EditProductComponent implements OnInit {
   products: any;
   baseUri = this.api.API_URL;
 
-  constructor(private api:ApiServiseService) { }
+  constructor(private api:ApiServiseService, private dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.getAllProducts();
@@ -26,8 +27,12 @@ export class EditProductComponent implements OnInit {
 
       )
   }
-  openChild(item: any){
-    console.log(item)
+  openChild(product: any) {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.data = product;
+    dialogConfig.width = '800px';
+    dialogConfig.height = '500px';
+    this.dialog.open(EditProductPopupComponent, dialogConfig);
   }
 
 }

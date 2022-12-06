10096d14fc63ef40a4325e3bd2b73df7cab27e61
migaa10 <migaraten@gmail.com>
2022-12-07 00:39:07 +0530

    add product edit popup v1

--- src/app/order/edit-product-popup/edit-product-popup.component.ts
+++ src/app/order/edit-product-popup/edit-product-popup.component.ts
@@ -14,7 +14,7 @@ export class EditProductPopupComponent implements OnInit {
   baseUri = this.api.API_URL;
   selectedFile: any;
 
-  constructor(private router: Router,public dialogRef: MatDialogRef<EditProductPopupComponent>, @Inject(MAT_DIALOG_DATA) public data: any, private api: ApiServiseService, private toastr: ToastrService) { }
+  constructor(private router: Router, public dialogRef: MatDialogRef<EditProductPopupComponent>, @Inject(MAT_DIALOG_DATA) public data: any, private api: ApiServiseService, private toastr: ToastrService) { }
 
   productItem = new FormGroup({
     product_id: new FormControl('', [Validators.required]),
@@ -22,15 +22,18 @@ export class EditProductPopupComponent implements OnInit {
     product_category: new FormControl('', [Validators.required, Validators.minLength(3)]),
     product_disc: new FormControl('', [Validators.required]),
     product_price: new FormControl('', [Validators.required]),
+    isPromo: new FormControl('', [Validators.required]),
+    product_promo_price: new FormControl('', [Validators.required]),
   })
   ngOnInit(): void {
-    console.log(this.data)
     this.productItem.setValue({
       product_id: this.data.product_id,
       product_name: this.data.product_name,
       product_category: this.data.product_category,
       product_disc: this.data.product_disc,
       product_price: this.data.product_price,
+      isPromo: this.data.isPromo,
+      product_promo_price: this.data.product_promo_price,
     })
   }
   saveNews() {
@@ -41,10 +44,16 @@ export class EditProductPopupComponent implements OnInit {
     fd.append("product_category", this.productItem.value['product_category'] || '');
     fd.append("product_disc", this.productItem.value['product_disc'] || '');
     fd.append("product_price", this.productItem.value['product_price'] || '');
+    fd.append("_id", this.data._id);
+    fd.append("isPromo", this.productItem.value['isPromo'] || '');
+    fd.append("product_promo_price", this.productItem.value['product_promo_price'] || '');
+
+    fd.append("product_imageuri", this.data.product_imageuri);
 
     this.api.getresponse("put", "product", fd)
       .subscribe(res => {
         this.toastr.success(res.msg);
+        this.dialogRef.close();
         // this.router.navigate(['/add_items'] , { queryParams: { id: res.data.product_id}});
       },
         err => console.log(err)
@@ -53,7 +62,6 @@ export class EditProductPopupComponent implements OnInit {
   };
   onFileSelected = (event: any) => {
     this.selectedFile = event.target.files[0];
-    console.log(this.selectedFile);
   };
   get f() {
     return this.productItem.controls;
